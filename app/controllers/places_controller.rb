b5122890b18ffa5a432a622e425b4ad844d67fe9
Jeff OConnell <jeffreytoconnell@gmail.com>
2017-12-08 20:43:16 -0800

    Updating the Place Data

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -27,6 +27,13 @@ class PlacesController < ApplicationController
         @places = Place.paginate(:page => params[:page], per_page: 3)
     end
 
+    def update
+        @place = Place.find(params[:id])
+        @place.update_attributes(place_params)
+        redirect_to root_path
+    end
+
+
 
     private
 
